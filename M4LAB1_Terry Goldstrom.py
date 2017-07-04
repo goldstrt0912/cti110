0e0d6434b96a2f59454968c518c7d1162f9c6a8e
goldstrt0912 <goldstrt0912@student.faytechcc.edu>
2017-07-04 10:44:22 -0400

    Module 4 - Shapes (Optional)

---
+++ M4LAB1_Terry Goldstrom.py
@@ -0,0 +1,28 @@
+# Shapes
+# 20 June 2017
+# CTI-110 M4LAB1 Shapes
+# Terry Goldstrom
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.color('yellow')
+turtle.width('10')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.color('purple')
+turtle.width('5')
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
