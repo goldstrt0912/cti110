8a7b1bf27a85aa7247d66b2b31afa212f268539c
goldstrt0912 <goldstrt0912@student.faytechcc.edu>
2017-07-04 10:44:50 -0400

    Module 4 - Initials (Optional)

---
+++ M4LAB2_Terry Goldstrom.py
@@ -0,0 +1,42 @@
+# Initials
+# 20 June 2017
+# CTI-110 MLAB2: Initials
+# Terry Goldstrom
+
+import turtle
+
+wn = turtle.Screen()
+terry = turtle.Turtle()
+wn.bgcolor('pink')
+
+terry.color('Purple')
+terry.width(20)
+terry.forward(100)
+terry.backward(50)
+terry.right(90)
+terry.forward(90)
+
+terry.penup()
+terry.left(90)
+terry.forward(150)
+terry.left(90)
+terry.pendown()
+
+terry.forward(30)
+terry.right(90)
+terry.forward(15)
+terry.left(180)
+terry.forward(30)
+terry.right(180)
+terry.forward(15)
+terry.right(90)
+terry.forward(30)
+terry.right(90)
+terry.forward(65)
+terry.right(90)
+terry.forward(90)
+terry.right(90)
+terry.forward(65)
+terry.right(90)
+terry.forward(30)
+
