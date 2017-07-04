c086da5e40da5d74a5e4251c6aba2348809ac461
goldstrt0912 <goldstrt0912@student.faytechcc.edu>
2017-07-04 10:45:50 -0400

    Module 4 - Snowflake (Optional)

---
+++ M4LAB3_Terry Goldstrom.py
@@ -0,0 +1,19 @@
+# Snowflake
+# 20 June 2017
+# CTI-110 M4LAB3: Snowflake
+# Terry Goldstrom
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('Yellow')
+turtle.color('Magenta')
+turtle.width('20')
+
+for i in range(10):
+    for i in range(2):
+        turtle.forward(100)
+        turtle.right(60)
+        turtle.forward(100)
+        turtle.right(120)
+    turtle.right(36)
